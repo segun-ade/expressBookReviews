cf000a0374d3168200ad0dee6b59d96fc0df6979
Adeyemi Segun <seguncongrat@gmail.com>
2026-03-14 16:25:12 -0400

    edited title

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -9,7 +9,7 @@ const doesExist = (username) => {
     // Filter the users array for any user with the same username
     let userswithsamename = users.filter((user) => {
         return user.username === username;
-    }); 
+    });  
     // Return true if any user with the same username is found, otherwise false
     if (userswithsamename.length > 0) {
         return true;
