698776341fa9e2600fdddc58bc4e163ae1d5632f
Adeyemi Segun <seguncongrat@gmail.com>
2026-03-14 12:23:06 -0400

    add promise

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -43,7 +43,13 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: JSON.stringify(books,null,4)});
+  let myPromise = new Promise((resolve,reject) => {
+    resolve(JSON.stringify(books,null,4))
+  })
+  myPromise.then((books_list) => {
+    return res.status(300).json({message: books_list});
+  })
+  //return res.status(300).json({message: JSON.stringify(books,null,4)});
   //res.send(JSON.stringify(books,null,4))
 });
 
@@ -51,9 +57,15 @@ public_users.get('/',function (req, res) {
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
   const isbn = req.params.isbn;
-  return res.status(300).json({message: books[isbn]});
+  let myPromise = new Promise((resolve,reject) => {
+    resolve(books[isbn])
+  })
+  myPromise.then((selected_book) => {
+    return res.status(300).json({message: selected_book});
+  })
+  //return res.status(300).json({message: books[isbn]});
  });
-  
+
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
@@ -61,14 +73,21 @@ public_users.get('/author/:author',function (req, res) {
     const author = req.params.author;
     const isbn_keys = Object.keys(books);
     let filtered_books = {};
+    let myPromise = new Promise((resolve,reject) => {
+        isbn_keys.forEach(isbn => {
+            if(books[isbn].author === author) {
+                filtered_books = books[isbn];
+            }
+        });
+        resolve(filtered_books)
+      })
+      myPromise.then((selected_book) => {
+        return res.status(300).json({message: selected_book});
+      })
     // Filter the books dictionary to find books whose author matches the extracted author parameter
-    isbn_keys.forEach(isbn => {
-        if(books[isbn].author === author) {
-            filtered_books = books[isbn];
-        }
-    });
+
     // Send the filtered_books dictionary as the response to the client
-  return res.status(300).json({message: filtered_books});
+  //return res.status(300).json({message: filtered_books});
 });
 
 // Get all books based on title
@@ -78,14 +97,21 @@ public_users.get('/title/:title',function (req, res) {
     const title = req.params.title;
     const isbn_keys = Object.keys(books);
     let filtered_books = {};
+    let myPromise = new Promise((resolve,reject) => {
+        isbn_keys.forEach(isbn => {
+            if(books[isbn].title === title) {
+                filtered_books = books[isbn];
+            }
+        });
+        resolve(filtered_books)
+      })
+      myPromise.then((selected_book) => {
+        return res.status(300).json({message: selected_book});
+      })
     // Filter the books dictionary to find books whose author matches the extracted author parameter
-    isbn_keys.forEach(isbn => {
-        if(books[isbn].title === title) {
-            filtered_books = books[isbn];
-        }
-    });
+
     // Send the filtered_books dictionary as the response to the client
-  return res.status(300).json({message: filtered_books});
+  //return res.status(300).json({message: filtered_books});
 });
 
 //  Get book review
