d6544210deb3dea8e424f275cb19e8967572cf07
Adeyemi Segun <seguncongrat@gmail.com>
2026-03-14 06:32:41 -0400

    add queries

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,31 +13,56 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  return res.status(300).json({message: JSON.stringify(books,null,4)});
+  //res.send(JSON.stringify(books,null,4))
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(300).json({message: books[isbn]});
  });
   
 // Get book details based on author
 public_users.get('/author/:author',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Extract the author parameter from the request URL
+    const author = req.params.author;
+    const isbn_keys = Object.keys(books);
+    let filtered_books = {};
+    // Filter the books dictionary to find books whose author matches the extracted author parameter
+    isbn_keys.forEach(isbn => {
+        if(books[isbn].author === author) {
+            filtered_books = books[isbn];
+        }
+    });
+    // Send the filtered_books dictionary as the response to the client
+  return res.status(300).json({message: filtered_books});
 });
 
 // Get all books based on title
 public_users.get('/title/:title',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    // Extract the title parameter from the request URL
+    const title = req.params.title;
+    const isbn_keys = Object.keys(books);
+    let filtered_books = {};
+    // Filter the books dictionary to find books whose author matches the extracted author parameter
+    isbn_keys.forEach(isbn => {
+        if(books[isbn].title === title) {
+            filtered_books = books[isbn];
+        }
+    });
+    // Send the filtered_books dictionary as the response to the client
+  return res.status(300).json({message: filtered_books});
 });
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const isbn = req.params.isbn;
+  return res.status(300).json({message: books[isbn].reviews});
 });
 
 module.exports.general = public_users;
